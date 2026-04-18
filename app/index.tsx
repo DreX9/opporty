33b112a2434ac4f44c06f4d8298d3cf669f63419
DreX9 <152118102+DreX9@users.noreply.github.com>
2026-04-18 17:48:17 -0500

    Cambios en el login integracion de formularios

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,8 @@ import Logo from '@/assets/icons/Logo';
 import { Box } from '@/components/ui/box';
 import { ScrollView } from 'react-native';
 import { Text } from '@/components/ui/text';
+import { Input, InputField } from '@/components/ui/input'
+import { LinearGradient } from 'expo-linear-gradient'
 
 //icon
 
@@ -16,22 +18,55 @@ import { ICONS } from '@/components/icons';
 export default function Home() {
   const router = useRouter();
   return (
-    <Box className="flex-1 bg-white dark:bg-background-500 items-center justify-center gap-6">
+    <Box className="flex-1 bg-background-500 items-center justify-center px-6">
 
-      {/* 🔵 Icono */}
-      <Box className="w-24 h-24 rounded-full bg-primary-500 items-center justify-center">
-        <Icon as={ICONS.webhook} className="w-12 h-12 text-black dark:text-black" />
+      {/* 🔵 HEADER */}
+      <Text className="text-2xl font-bold text-white mb-6">
+        AETHERIS <Text className="text-primary-500">PULSE</Text>
+      </Text>
+
+      {/* 🧱 CARD */}
+      <Box className="w-full bg-[#1A2235] p-6 rounded-3xl flex-[0.5] justify-center">
+
+        {/* INPUT EMAIL */}
+        <Box className='flex-row'>
+          <Icon as={ICONS.lock} className='pr-10'/>
+        <Text className="text-gray-400 mb-2">Operator Identity</Text>
+        </Box>
+        <Input className="mb-4 h-16 rounded-2xl">
+          <InputField placeholder="ID / Email" />
+        </Input>
+
+        {/* INPUT PASSWORD */}
+        <Text className="text-gray-400 mb-2">Access Protocol</Text>
+        <Input className="mb-6 h-16 rounded-2xl">
+          <InputField placeholder="••••••••••" secureTextEntry />
+        </Input>
+
+        {/* 🔥 BOTÓN CON GRADIENT */}
+        <LinearGradient
+          colors={['#00E5FF', '#00B8D4']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={{
+           borderRadius: 8,
+  overflow: 'hidden',
+          }}
+        >
+          <Button className="bg-transparent w-full"
+          variant="solid" size="xl"
+          onPress={() => router.push('/tabs/tab1')}
+
+          
+          >
+            <ButtonText className="text-black font-bold">
+              Inicie Sesión
+            </ButtonText>
+          </Button>
+        </LinearGradient>
+          
       </Box>
-
-      {/* 🔘 Botón */}
-      <Button
-        className="bg-primary-500 px-6 py-2 rounded-full"
-        onPress={() => router.push('/tabs/tab1')}
-      >
-        <ButtonText className="text-black dark:text-black">
-          Ir a Tabs
-        </ButtonText>
-      </Button>
+      <Text className='p-6'>Net to the radar network?</Text>
 
     </Box>
   );
